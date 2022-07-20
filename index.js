f740a8e0bc33c1ae42d237fe187a8f7b742a79c2
wpshamim <wpshamim@gmail.com>
2022-07-20 22:58:06 +0600

    get order api added

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+const jwtVerify = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Error 401 - Unauthorized!" });
+    }
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN, async (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Error 403 - Forbidden" });
+        }
+        req.decoded = decoded;
+        await next();
+    });
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofcq8yt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const toolsCollection = client.db("dewalt").collection("tools");
+        const userCollection = client.db("dewalt").collection("users");
+        const orderCollection = client.db("dewalt").collection("orders");
+
+        // Authentication by JWT and update new user
+
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    email: user?.email,
+                    role: "user",
+                },
+            };
+
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, {
+                expiresIn: "1d",
+            });
+            res.send({ result, token });
+        });
+
+        app.get("/tools", jwtVerify, async (req, res) => {
+            const tools = await toolsCollection.find({}).toArray();
+            res.send(tools);
+        });
+
+        app.get("/tool/:id", jwtVerify, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        });
+
+        app.post("/order", async (req, res) => {
+            const orderDetails = req.body;
+            const result = await orderCollection.insertOne(orderDetails);
+            res.send(result);
+        });
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
