6e76ff342979b756daf0ca233e56988203576307
wpshamim <wpshamim@gmail.com>
2022-07-22 22:02:50 +0600

    install heroku and create heroku project

--- index.js
+++ index.js
@@ -250,5 +250,5 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Welcome to DEWALT-BD Server"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
